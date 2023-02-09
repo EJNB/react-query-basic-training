ebe91f6dd2806b4fdf7e0de837f62da7f41caf3e
Javier <javiernieto1989@gmail.com>
2023-02-09 12:34:26 -0500

    Added selected and change selected labels

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,25 +1,38 @@
 import { useLabels } from "../hooks/useLabels";
 import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { FC } from "react";
+import { Label } from "../interfaces/label";
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChangeSelectedLabel: (labelName: string) => void;
+}
+
+export const LabelPicker: FC<Props> = ({
+  selectedLabels,
+  onChangeSelectedLabel,
+}) => {
   const labelsQuery = useLabels();
 
   if (labelsQuery.isLoading) return <LoadingIcon />;
 
   return (
-    <div>
+    <>
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : ""
+          }`}
           style={{
             border: `1px solid #${label.color}`,
             color: `${label.color}`,
           }}
+          onClick={() => onChangeSelectedLabel(label.name)}
         >
           {label.name}
         </span>
       ))}
-    </div>
+    </>
   );
 };
