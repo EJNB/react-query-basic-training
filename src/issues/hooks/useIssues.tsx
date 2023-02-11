cf1e7687331d263b9f6ce3bb302a9282bce4bf68
Javier <javiernieto1989@gmail.com>
2023-02-11 12:12:47 -0500

    Implemented complex filter by status using useQuery hook

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,13 +1,34 @@
 import { githubApi } from "../../api/githubApi";
-import { Issue, Label } from "../interfaces";
+import { Issue, Label, State } from "../interfaces";
 import { useQuery } from "@tanstack/react-query";
+import { sleep } from "../../helpers/sleep";
 
-const getIssues = async (): Promise<Issue[]> => {
-  const { data } = await githubApi.get<Issue[]>("/issues");
+interface Props {
+  state?: State;
+  labels: string[];
+}
+
+const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
+  sleep(2);
+
+  const params = new URLSearchParams();
+  if (state) params.append("state", state);
+
+  if (labels.length > 0) {
+    const labelString = labels.join(","); // label 1, label 2, label 3
+    params.append("labels", labelString);
+  }
+
+  params.append("page", "1");
+  params.append("per_page", "5");
+
+  const { data } = await githubApi.get<Issue[]>("/issues", { params });
   return data;
 };
 
-export const useIssues = () => {
-  const issuesQuery = useQuery(["issues"], getIssues);
+export const useIssues = ({ state, labels }: Props) => {
+  const issuesQuery = useQuery(["issues", { state, labels }], () =>
+    getIssues(labels, state)
+  );
   return { issuesQuery };
 };
